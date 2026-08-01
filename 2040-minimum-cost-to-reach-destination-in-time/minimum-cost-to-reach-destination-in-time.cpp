bf2fcf25e6190e052cfb682ca2f680894a15d4a5
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-01 14:30:07 +0530

    Time: 119 ms (78.31%) | Memory: 43.2 MB (81.32%) - LeetSync

---
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int minCost(int maxTime, vector<vector<int>>& edges,
+                vector<int>& passingFees) {
+
+        int n = passingFees.size();
+
+        vector<pair<int,int>> adj[n];
+        for (auto &e : edges) {
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
+        }
+
+        vector<int> minTime(n, INT_MAX);
+        vector<int> minCost(n, INT_MAX);
+
+        priority_queue<
+            vector<int>,
+            vector<vector<int>>,
+            greater<vector<int>>
+        > pq;
+
+        // {cost, time, node}
+        pq.push({passingFees[0], 0, 0});
+
+        minTime[0] = 0;
+        minCost[0] = passingFees[0];
+
+        while (!pq.empty()) {
+
+            auto cur = pq.top();
+            pq.pop();
+
+            int cost = cur[0];
+            int time = cur[1];
+            int node = cur[2];
+
+            if (node == n - 1)
+                return cost;
+
+            for (auto &it : adj[node]) {
+
+                int nxt = it.first;
+                int t = it.second;
+
+                int newTime = time + t;
+                int newCost = cost + passingFees[nxt];
+
+                if (newTime > maxTime)
+                    continue;
+
+                if (newTime < minTime[nxt]) {
+                    minTime[nxt] = newTime;
+                    minCost[nxt] = newCost;
+                    pq.push({newCost, newTime, nxt});
+                }
+                else if (newCost < minCost[nxt]) {
+                    minCost[nxt] = newCost;
+                    pq.push({newCost, newTime, nxt});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
