b1b3d76d081b1617e7c3119252e5e167a9976290
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-01 14:36:31 +0530

    Time: 120 ms (77.72%) | Memory: 43.1 MB (81.46%) - LeetSync

--- 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.cpp
@@ -1,3 +1,67 @@
+// class Solution {
+// public:
+//     int minCost(int maxTime, vector<vector<int>>& edges,
+//                 vector<int>& passingFees) {
+
+//         int n = passingFees.size();
+
+//         vector<pair<int,int>> adj[n];
+//         for (auto &e : edges) {
+//             adj[e[0]].push_back({e[1], e[2]});
+//             adj[e[1]].push_back({e[0], e[2]});
+//         }
+
+//         vector<vector<int>> dist(n, vector<int>(maxTime + 1, INT_MAX));
+
+//         priority_queue<
+//             vector<int>,
+//             vector<vector<int>>,
+//             greater<vector<int>>
+//         > pq;
+
+//         // {cost, time, node}
+//         pq.push({passingFees[0], 0, 0});
+//         dist[0][0] = passingFees[0];
+
+//         while (!pq.empty()) {
+
+//             auto cur = pq.top();
+//             pq.pop();
+
+//             int cost = cur[0];
+//             int time = cur[1];
+//             int node = cur[2];
+
+//             if (node == n - 1)
+//                 return cost;
+
+//             if (cost > dist[node][time])
+//                 continue;
+
+//             for (auto &it : adj[node]) {
+
+//                 int nxt = it.first;
+//                 int edgeTime = it.second;
+
+//                 int newTime = time + edgeTime;
+//                 if (newTime > maxTime)
+//                     continue;
+
+//                 int newCost = cost + passingFees[nxt];
+
+//                 if (newCost < dist[nxt][newTime]) {
+//                     dist[nxt][newTime] = newCost;
+//                     pq.push({newCost, newTime, nxt});
+//                 }
+//             }
+//         }
+
+//         return -1;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int minCost(int maxTime, vector<vector<int>>& edges,
