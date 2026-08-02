a9d5972078acf4a0aab971eeb23f3d30816378ea
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-03 03:07:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (36.26%) - LeetSync

--- 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int maxRepeating(string sequence, string word) {
-
         string cur = word;
         int ans = 0;
-
         while (sequence.find(cur) != string::npos) {
             ans++;
             cur += word;
         }
-
         return ans;
     }
 };
