2f362bfb56a6d7dc403f192cfc012c2c9c4ea6c3
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-11 10:32:00 +0530

    Time: 211 ms (53.92%) | Memory: 115.3 MB (69.66%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,134 +1,118 @@
+
+
 class DisjointSet {
 
 public:
     vector<int> parent, rank, size;
-
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
         size.resize(n + 1);
-
-        for(int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             parent[i] = i;
             size[i] = 1;
         }
     }
-
     int findpar(int node) {
-        if(node == parent[node])
+        if (node == parent[node]) {
             return node;
-
+        }
         return parent[node] = findpar(parent[node]);
     }
-
-    void unionBySize(int u, int v) {
-
+    void unionByRank(int u, int v) {
         int ulp_u = findpar(u);
         int ulp_v = findpar(v);
-
-        if(ulp_u == ulp_v)
+        if (ulp_u == ulp_v)
             return;
-
-        if(size[ulp_u] < size[ulp_v]) {
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v) {
+        int ulp_u = findpar(u);
+        int ulp_v = findpar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v]) {
             parent[ulp_u] = ulp_v;
             size[ulp_v] += size[ulp_u];
-        }
-        else {
+        } else {
             parent[ulp_v] = ulp_u;
             size[ulp_u] += size[ulp_v];
         }
     }
 };
-
 class Solution {
-
 private:
-
     bool isvalid(int nr, int nc, int n, int m) {
         return nr >= 0 && nr < n && nc >= 0 && nc < m;
     }
 
 public:
-
     int largestIsland(vector<vector<int>>& grid) {
-
         int n = grid.size();
-
+        int ans = 0;
         DisjointSet ds(n * n);
-
-        int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
-
-        // Build DSU for all existing islands
-        for(int r = 0; r < n; r++) {
-
-            for(int c = 0; c < n; c++) {
-
-                if(grid[r][c] == 0)
-                    continue;
-
-                for(int i = 0; i < 4; i++) {
-
-                    int nr = r + dr[i];
-                    int nc = c + dc[i];
-
-                    if(isvalid(nr, nc, n, n) && grid[nr][nc] == 1) {
-
+        int dr[] = {-1, 0, +1, 0};
+        int dc[] = {0, +1, 0, -1};
+        for (int i = 0;i<n;i++) {
+            for(int j = 0;j<n;j++){
+            int r = i;
+            int c = j;
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                if (grid[r][c] == 1) {
+                    if (isvalid(nr, nc, n, n) && grid[nr][nc] == 1) {
                         int val1 = r * n + c;
                         int val2 = nr * n + nc;
-
-                        if(ds.findpar(val1) != ds.findpar(val2)) {
-                            ds.unionBySize(val1, val2);
-                        }
+                        int n1 = ds.findpar(val1);
+                        int n2 = ds.findpar(val2);
+                        if (n1 != n2)
+                            ds.unionBySize(n1, n2);
+                        else
+                            continue;
                     }
                 }
             }
         }
-
-        int ans = 0;
-        bool hasZero = false;
-
-        // Try converting every 0 into 1
-        for(int r = 0; r < n; r++) {
-
-            for(int c = 0; c < n; c++) {
-
-                if(grid[r][c] == 1)
-                    continue;
-
-                hasZero = true;
-
-                set<int> st;
-
-                int temp = 1; 
-
-                for(int i = 0; i < 4; i++) {
-
+        }
+            int h = 0;
+            for (int i = 0;i<n;i++){
+                for(int j = 0;j<n;j++)
+             {
+                int r = i;
+                int c = j;
+                int temp = 0;
+                set<int>st;
+                for (int i = 0; i < 4; i++) {
                     int nr = r + dr[i];
                     int nc = c + dc[i];
+                    if (grid[r][c] == 1)
+                        continue;
 
-                    if(isvalid(nr, nc, n, n) && grid[nr][nc] == 1) {
+                    else if (grid[r][c] == 0) {
+                        h++;
+                        if (isvalid(nr, nc, n, n) && grid[nr][nc] == 1) {
 
-                        int val = nr * n + nc;
-
-                        int node = ds.findpar(val);
-
-                        if(st.find(node) == st.end()) {
-
-                            st.insert(node);
-
+                            int val = nr * n + nc;
+                            int node = ds.findpar(val);
+                            if(st.find(node)==st.end()){
                             temp += ds.size[node];
+                            st.insert(node);
+                            }
                         }
                     }
                 }
-
-                ans = max(ans, temp);
+                ans = max(temp + 1, ans);
             }
+            }
+            if(h==0) return n*n;
+            return ans;
         }
-
-        if(!hasZero)
-            return n * n;
-
-        return ans;
-    }
-};
+    };
