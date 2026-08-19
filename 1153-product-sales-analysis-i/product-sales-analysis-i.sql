50e6cff383be18dbe6f06417e80be64db71c22d7
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-19 15:58:43 +0530

    Time: 1242 ms (76.47%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -3,4 +3,4 @@
 -- product_name,year,price from Sales as s inner join Product as p on s.product_id = p.product_id;
 
 select 
-product_name,year,price from Sales as s inner join Product as p using(product_id);
+product_name,year,price from Sales as s left join Product as p using(product_id);
