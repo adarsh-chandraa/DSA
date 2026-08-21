87748f0d180800c66ae684891679e077a8a6eb44
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-21 11:42:04 +0530

    Time: 1191 ms (8.79%) | Memory: 93.9 MB (68.79%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -4,7 +4,7 @@ public:
 
         sort(intervals.begin(), intervals.end(),
              [](const vector<int>& a, const vector<int>& b) {
-                if(a[1]==b[1]) return a[0]<b[0];
+                //if(a[1]==b[1]) return a[0]<b[0];
                  return a[1] < b[1];
              });
         for(auto &it:intervals){
