e97967e6fd5591260a5a5433ac2f33d5d25d277b
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-23 23:58:44 +0530

    Time: 487 ms (65.17%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
+
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
 ORDER BY count(order_number) DESC
 LIMIT 1;
 
--- select customer_number  as customer_number  from (select customer_number,count(customer_number ) as number from Orders group by customer_number) as temp where temp.number = max(temp.number);
 
+
+-- second largest customer_number
+-- select customer_number from Orders group by customer_number order by customer_number desc limit 2 offset 1;
+-- limit means howm many row you wnat to take
+-- offset means how many you want to skip.
