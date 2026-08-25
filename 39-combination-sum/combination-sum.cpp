6475bc53e8dc9920cc8aecbfec4ff973275dfe95
Adarsh Kumar <kadarshchandra@gmail.com>
2026-08-25 23:36:41 +0530

    Time: 28 ms (13.77%) | Memory: 14.2 MB (59.06%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -3,17 +3,18 @@ public:
     void solve(vector<int>& arr,int tar,vector<vector<int>>&ans,vector<int>&temp,int i,int sum){
         if(i>arr.size()) return ;
         if(tar == sum) {
-            cout<<"->"<<sum;
+            //cout<<"->"<<sum;
             ans.push_back(temp);
             return;
         }
-         if(sum > tar) return;
+         
         for(int j= i;j<arr.size();j++){
             if(j>i && arr[j]==arr[j-1]) continue;
             sum = sum + arr[j];
-           
+            
+            if(sum > tar) break;
+            cout<<"->"<<sum;
             temp.push_back(arr[j]);
-           // cout<<"->"<<sum;
             solve(arr,tar,ans,temp,j,sum);
             sum = sum - arr[j];
             temp.pop_back();
