9983341234a2b9cbee261dec67a55c65abb76545
Adarsh Kumar <kadarshchandra@gmail.com>
2026-09-03 11:50:39 +0530

    Time: 4 ms (65.18%) | Memory: 165.8 MB (62.83%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -3,6 +3,9 @@ public:
     bool uniformArray(vector<int>& nums1) {
         int mini = *min_element(nums1.begin(),nums1.end());
         if(mini%2 != 0) return true;
+        // minimum odd hoga toh sara odd bna denge
+        //agar even hua toh sara element even hona chahiye kyu ki  
+        // even agar sabse chota hoga togh odd nhi bna skte h sare kro agar rest element odd bhi hua toh bhi 7 9 5 2 ----> false 2- something will give -ve
         for(auto &it:nums1){
             if(it%2 !=0) return false;
         }
