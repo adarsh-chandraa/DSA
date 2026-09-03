e9abd059259179662dc9a428257d3934296603ff
Adarsh Kumar <kadarshchandra@gmail.com>
2026-09-03 11:41:43 +0530

    Time: 0 ms (100.00%) | Memory: 165.9 MB (31.94%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mini = *min_element(nums1.begin(),nums1.end());
+        if(mini%2 != 0) return true;
+        for(auto &it:nums1){
+            if(it%2 !=0) return false;
+        }
+        return true;
+    }
+};
